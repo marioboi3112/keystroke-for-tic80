40a8fbb84db8ad69cb2cc11f2900022764417025
obi3112 <drazeobi3112@gmail.com>
2023-09-03 16:08:21 -0500

    Making the keystroke accessible as a class.

--- keystroke.lua
+++ keystroke.lua
@@ -2,20 +2,21 @@
 --author:marioboi3112
 --desc: simple code to make keystrokes for TIC-80
 
-
-
-local keys = { --variables for the keystroke
-			wX = 10,
-			wY = 4,
-			aX = 4,
-			aY = 10,
-			sX = 10,
-			sY = 10,
-			dX = 16,
-			dY = 10
-}
-function keystroke()
-    local wBox = rect(keys.wX, keys.wY, 5, 5, 2)
+--UPDATE: code can now be accessed as a class for more flexibilty or you can edit the table keys yourself :)
+function Keystroke()
+return{
+	local keys = { --variables for the keystroke
+		wX = 10,
+		wY = 4,
+		aX = 4,
+		aY = 10,
+		sX = 10,
+		sY = 10,
+		dX = 16,
+		dY = 10
+	},
+	render = function(self)
+		local wBox = rect(keys.wX, keys.wY, 5, 5, 2)
     local aBox = rect(keys.aX, keys.aY, 5, 5, 2)
     local sBox = rect(keys.sX, keys.sY, 5, 5, 2)
     local dBox = rect(keys.dX, keys.dY, 5, 5, 2)
@@ -28,7 +29,10 @@ function keystroke()
    if key(1) then print("A", keys.aX,keys.aY,0)end
    if key(19) then print("S", keys.sX,keys.sY,0)end
    if key(4) then print("D", keys.dX,keys.dY,0)end
+	end
+}
 end
-function TIC()
-  keystroke() --call the keystroke function
-end
+
+cls(12)
+local keystroke = Keystroke()
+keystroke:render()
