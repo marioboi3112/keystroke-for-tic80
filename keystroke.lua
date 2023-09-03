ae378535913041c165007eda99dd3aaaae946756
obi3112 <drazeobi3112@gmail.com>
2023-09-03 16:17:05 -0500

    Making the keystroke accessible as a class.

--- keystroke.lua
+++ keystroke.lua
@@ -2,38 +2,39 @@
 --author:marioboi3112
 --desc: simple code to make keystrokes for TIC-80
 
---UPDATE: code can now be used as a library meaning all you need to do is require this file,
--- make a new instance of the keystroke class and call the render method on the instane :)
+--UPDATE: code can now be accessed as a class for more flexibilty or you can edit the table keys yourself :)
+
 function Keystroke()
-return{
-	local keys = { --variables for the keystroke
-		wX = 10,
-		wY = 4,
-		aX = 4,
-		aY = 10,
-		sX = 10,
-		sY = 10,
-		dX = 16,
-		dY = 10
-	},
-	render = function(self)
-		local wBox = rect(keys.wX, keys.wY, 5, 5, 2)
-    local aBox = rect(keys.aX, keys.aY, 5, 5, 2)
-    local sBox = rect(keys.sX, keys.sY, 5, 5, 2)
-    local dBox = rect(keys.dX, keys.dY, 5, 5, 2)
-		w =	print("W", keys.wX,keys.wY,12)
-		a = print("A", keys.aX,keys.aY,12)
-		s = print("S", keys.sX,keys.sY,12)
-    d = print("D", keys.dX,keys.dY,12)
-
-   if key(23) then print("W", keys.wX,keys.wY,0)end
-   if key(1) then print("A", keys.aX,keys.aY,0)end
-   if key(19) then print("S", keys.sX,keys.sY,0)end
-   if key(4) then print("D", keys.dX,keys.dY,0)end
+	return{
+		--variables for the keystroke
+			wX = 10,
+			wY = 4,
+			aX = 4,
+			aY = 10,
+			sX = 10,
+			sY = 10,
+			dX = 16,
+			dY = 10,
+		render = function(self)
+			local wBox = rect(self.wX, self.wY, 5, 5, 2)
+			local aBox = rect(self.aX, self.aY, 5, 5, 2)
+			local sBox = rect(self.sX, self.sY, 5, 5, 2)
+			local dBox = rect(self.dX, self.dY, 5, 5, 2)
+			w =	print("W", self.wX,self.wY,12)
+			a = print("A", self.aX,self.aY,12)
+			s = print("S", self.sX,self.sY,12)
+			d = print("D", self.dX,self.dY,12)
+	
+		 if key(23) then print("W", self.wX,self.wY,0)end
+		 if key(1) then print("A", self.aX,self.aY,0)end
+		 if key(19) then print("S",self.sX,self.sY,0)end
+		 if key(4) then print("D", self.dX,self.dY,0)end
+		end
+	}
 	end
-}
-end
-
-cls(12)
-local keystroke = Keystroke()
-keystroke:render()
+	
+	function TIC()
+	cls(3)
+	local keystroke = Keystroke()
+	keystroke:render()	
+	end
